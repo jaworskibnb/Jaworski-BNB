0ca0ba7d730a15d960c1e565e5622671e65fdf2b
jaworskibnb <jaworskibnb@gmail.com>
2026-03-30 23:51:45 -0400

    Add files via upload

---
+++ netlify/functions/get-stripe-key.js
@@ -0,0 +1,9 @@
+exports.handler = async () => {
+  return {
+    statusCode: 200,
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      publishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+    }),
+  };
+};
